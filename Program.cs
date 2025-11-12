66655b37426c68953f43c4a6e887bdc85d72d7b5
Arley <arleycordova2015@gmail.com>
2025-11-11 21:00:46 -0500

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MiPrimerPrograma
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Como te llamas");
+            string nombre = Console.ReadLine();
+            
+            DateTime hoy = DateTime.Now;
+            
+            Console.WriteLine("Hola " + nombre + ", bienvenido a C#");
+            Console.WriteLine("Hoy es " + hoy.ToString("dd/MM/yyyy"));
+            
+            Console.WriteLine("\nIngresa el primer numero");
+            int num1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingresa el segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+            
+            int suma = num1 + num2;
+            
+            Console.WriteLine("La suma es: " + suma);
+            
+            Console.ReadKey();
+        }
+    }
+}
